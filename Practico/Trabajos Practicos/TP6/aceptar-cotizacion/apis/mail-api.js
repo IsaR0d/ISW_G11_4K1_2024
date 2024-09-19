c93210a386de7f7142c6f8080aad5468f7d638c6
Maximo Sanchez <maximo.sanchez@darwoft.com>
2024-09-18 23:37:55 -0300

    mensajeria mail nodeemailer

--- Practico/Trabajos Practicos/TP6/aceptar-cotizacion/apis/mail-api.js
+++ Practico/Trabajos Practicos/TP6/aceptar-cotizacion/apis/mail-api.js
@@ -1,29 +1,40 @@
 require('dotenv').config();
 
 const express = require('express');
-const sgMail = require('@sendgrid/mail');
+const nodemailer = require('nodemailer');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+
 const app = express();
 app.use(cors());
 const port = process.env.PORT || 5000;
 
-sgMail.setApiKey("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");
-
 app.use(bodyParser.json());
+
+// Create reusable transporter object using Hotmail SMTP
+let transporter = nodemailer.createTransport({
+    service: 'hotmail', // Change service to Hotmail
+    auth: {
+        user: "tangoappisw@hotmail.com", // Replace with your Hotmail email address from environment variables
+        pass: "159951ISW"  // Replace with your Hotmail password or app password from environment variables
+    },
+    tls: {
+        rejectUnauthorized: false // Sometimes needed for self-signed certificates
+    }
+});
 
 app.post('/enviar-correo', async (req, res) => {
     const { detallesPedido } = req.body;
 
-    const msg = {
-        to: "agostina.avalle@gmail.com",
-        from: 'avalleag@gmail.com',
+    const mailOptions = {
+        from: "tangoappisw@hotmail.com", // Sender address
+        to: 'maxgamercod@gmail.com', // Receiver email
         subject: 'TANGO APP - Confirmación de Cotización',
-        text: detallesPedido,
+        text: detallesPedido // Email body content
     };
 
     try {
-        await sgMail.send(msg);
+        await transporter.sendMail(mailOptions);
         res.status(200).send('Correo electrónico enviado');
     } catch (error) {
         console.error('Error al enviar el correo electrónico:', error);
